94caabef133213cd4397c2362f96dddb1d9ed5f7
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-08-05 13:56:41 -0300

    Added html webpack plugin and renamed some files

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
 const config = {
     devtool: 'inline-source-map',
-    entry: './src/scripts/script.ts',
+    entry: './src/classes/Test.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    resolve:{
-        extensions: ['.ts','.tsx','.js']
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
     },
     module: {
         rules: [
             { test: /\.txt$/, use: 'raw-loader' },
-            { test: /\.tsx?$/, loader: 'ts-loader'}
+            { test: /\.tsx?$/, loader: 'ts-loader' }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ]
 }
 
 module.exports = config
